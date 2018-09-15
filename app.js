5e5737366c73555e8c405e93a0fa34313b5b313d
Guru Mohan Reddy <gurumohan449@gmail.com>
2018-09-15 10:46:31 +0530

    Introduce heal and logs

--- app.js
+++ app.js
@@ -3,29 +3,41 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameRunning: false
+    gameRunning: false,
+    logs: []
   },
   methods: {
     startGame: function() {
       this.gameRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.logs = [];
     },
     stopGame: function() {
       this.gameRunning = false;
-      this.playerHealth = 100;
-      this.monsterHealth = 100;
     },
     attack: function(){
       this.playerAttack(10);
+      if(this.gameOver()) {
+        return;
+      }
       this.monsterAttack();
     },
     specialAttack: function(){
       this.playerAttack(20);
+      if(this.gameOver()) {
+        return;
+      }
       this.monsterAttack();
     },
     heal: function(){
-
+      if(this.playerHealth <= 90){
+        this.playerHealth += 10
+        this.logs.unshift({isPlayer: true, value: 'Player healed by 10'});
+      }else{
+        this.playerHealth = 100;
+      }
+      this.monsterAttack();
     },
     gameOver: function() {
       if(this.playerHealth <= 0){
@@ -46,13 +58,15 @@ new Vue({
       return false;
     },
     playerAttack: function(attack) {
-      this.monsterHealth -= Math.floor(Math.random()*attack)+1;
-      if(this.gameOver()) {
-        return;
-      }
+      var a = Math.floor(Math.random()*attack)+1;
+      this.monsterHealth -= a
+      var value = attack == 10 ? 'Player attacked the moster with '+a : 'Player special attacked the moster with '+a
+      this.logs.unshift({isPlayer: true, value: value }); 
     },
     monsterAttack: function() {
-      this.playerHealth -= Math.floor(Math.random()*10)+3;
+      var a = Math.floor(Math.random()*10)+3;
+      this.playerHealth -= a;
+      this.logs.unshift({isPlayer: false, value: 'Monster attacked the player with '+a }); 
       this.gameOver();
     }
   }
